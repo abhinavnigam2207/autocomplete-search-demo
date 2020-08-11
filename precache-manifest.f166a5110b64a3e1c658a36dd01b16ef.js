6d17bdae2211696d058c6ca6cb1535b7e27abd23
Abhinav Nigam <abhinavnigam2207@gmail.com>
2020-08-11 14:31:13 +0530

    Updates

---
+++ precache-manifest.f166a5110b64a3e1c658a36dd01b16ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307cf17bf7f2cfa1cc61ca3e260e01ec",
+    "url": "/locus-assignment/index.html"
+  },
+  {
+    "revision": "0ac635484a4777118845",
+    "url": "/locus-assignment/static/css/main.d33b0bca.chunk.css"
+  },
+  {
+    "revision": "bd2bd8b8e2050e9ec7bd",
+    "url": "/locus-assignment/static/js/2.36aa39a6.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/locus-assignment/static/js/2.36aa39a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac635484a4777118845",
+    "url": "/locus-assignment/static/js/main.8858345e.chunk.js"
+  },
+  {
+    "revision": "c30eb132427f42402028",
+    "url": "/locus-assignment/static/js/runtime-main.8cbd66f0.js"
+  }
+]);

4746f45afbbcffc86a0d9354d2a4661dbdeef26e
Abhinav Nigam <abhinavnigam2207@gmail.com>
2020-08-11 14:35:14 +0530

    Request moved to https

--- src/services/users.js
+++ src/services/users.js
@@ -2,7 +2,7 @@
 import { validateResponse } from '../network'
 
 export const getUsers = async () => {
-  return fetch('http://www.mocky.io/v2/5ba8efb23100007200c2750c')
+  return fetch('https://www.mocky.io/v2/5ba8efb23100007200c2750c')
     .then(validateResponse)
     .then((resp) => resp.data);
 };

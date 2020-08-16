3f8a08401d28d989a27d012f3faea2564da65760
Abhinav Nigam <abhinavnigam2207@gmail.com>
2020-08-17 03:39:24 +0530

    Rename stuff

--- src/components/core/App.js
+++ src/components/core/App.js
@@ -5,7 +5,7 @@ import SearchPage from '../searchPage';
 function App() {
   return (
     <div className="text-center">
-      <header><h1>Locus Assignment</h1></header>
+      <header><h1>Autocomplete search Demo</h1></header>
       <SearchPage />
     </div>
   );
--- src/components/core/App.test.js
+++ src/components/core/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Locus Assignment/i);
+  const linkElement = getByText(/Autocomplete search Demo/i);
   expect(linkElement).toBeInTheDocument();
 });
